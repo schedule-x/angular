164ab2c0e49002ae2aeb5ce21e33a89411b3dc2d
Dave <77780263+BitsAndDroids@users.noreply.github.com>
2025-10-25 17:00:05 +0200

    feat: v3 and Angular 20 update (#127)
    BREAKING CHANGE: bumps dependency on @schedule-x/calendar to 3.0.0

--- projects/development/src/app/app.component.ts
+++ projects/development/src/app/app.component.ts
@@ -1,81 +1,78 @@
-import {Component, Inject, PLATFORM_ID} from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CalendarComponent} from "../../../schedule-x/angular/src/lib/calendar.component";
-import {createCalendar, viewWeek, viewMonthGrid, viewMonthAgenda} from "@schedule-x/calendar";
-import {isPlatformBrowser, NgIf} from '@angular/common';
-import '@schedule-x/theme-default/dist/calendar.css'
-import {createEventModalPlugin} from "@schedule-x/event-modal";
-import {createDragAndDropPlugin} from "@schedule-x/drag-and-drop";
+import { CalendarComponent } from '../../../schedule-x/angular/src/lib/calendar.component';
+import {
+  createCalendar,
+  CalendarApp,
+  createViewWeek,
+  createViewMonthGrid,
+  createViewMonthAgenda,
+} from '@schedule-x/calendar';
+import { isPlatformBrowser } from '@angular/common';
+import '@schedule-x/theme-default/dist/calendar.css';
+import { createEventModalPlugin } from '@schedule-x/event-modal';
+import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop';
+import 'temporal-polyfill/global';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, CalendarComponent, NgIf],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [RouterOutlet, CalendarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'development';
-  calendarApp = createCalendar({
-    events: [
-      {
-        id: '1',
-        title: 'Event 1',
-        start: '2024-06-11 08:00',
-        end: '2024-06-11 09:00',
-      },
-      {
-        id: '2',
-        title: 'Event 2',
-        start: '2024-06-11 10:00',
-        end: '2024-06-11 11:00',
-      },
-      {
-        id: '3',
-        title: 'Event 3',
-        start: '2024-06-11',
-        end: '2024-06-11',
-      },
-      {
-        id: '4',
-        title: 'Event 4',
-        start: '2024-06-11',
-        end: '2024-06-13',
-      }
-    ],
-    views: [viewWeek, viewMonthGrid, viewMonthAgenda],
-    plugins: [
-      createEventModalPlugin(),
-      createDragAndDropPlugin()
-    ]
-  })
+  calendarApp: CalendarApp;
+  constructor(@Inject(PLATFORM_ID) private platformId: string) {}
 
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: string,
-  ){}
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.calendarApp = createCalendar({
+        showWeekNumbers: true,
+        events: [
+          {
+            id: '1',
+            title: 'Event 1',
+            start: Temporal.Now.zonedDateTimeISO(),
+            end: Temporal.Now.zonedDateTimeISO().add({ hours: 1 }),
+          },
+
+          {
+            id: '2',
+            title: 'Event 2',
+            start: Temporal.Now.zonedDateTimeISO().add({ days: 1 }),
+            end: Temporal.Now.zonedDateTimeISO().add({ days: 1, hours: 1 }),
+          },
+        ],
+        views: [
+          createViewWeek(),
+          createViewMonthGrid(),
+          createViewMonthAgenda(),
+        ],
+        plugins: [createEventModalPlugin(), createDragAndDropPlugin()],
+      });
+    }
+  }
 
   get isBrowserOnly(): boolean {
     return isPlatformBrowser(this.platformId);
   }
 
   setEvents() {
-    // const allEvents = this.calendarApp.events.getAll()
-    // allEvents.forEach(event => {
-    //   event.title = 'New Title'
-    // })
-    // this.calendarApp.events.set(allEvents)
     this.calendarApp.events.set([
       {
         id: '5',
         title: 'Event 5',
-        start: '2024-06-11 08:00',
-        end: '2024-06-11 09:00',
+        start: Temporal.Now.plainDateISO().add({ days: 2 }),
+        end: Temporal.Now.plainDateISO().add({ days: 2, hours: 1 }),
       },
+
       {
         id: '6',
         title: 'Event 6',
-        start: '2024-06-11 10:00',
-        end: '2024-06-11 11:00',
+        start: Temporal.Now.zonedDateTimeISO().add({ days: 1 }),
+        end: Temporal.Now.zonedDateTimeISO().add({ days: 1, hours: 1 }),
       },
-    ])
+    ]);
   }
 }
--- projects/schedule-x/angular/src/lib/calendar.component.ts
+++ projects/schedule-x/angular/src/lib/calendar.component.ts
@@ -3,35 +3,36 @@ import {
   Component,
   ContentChild,
   Input,
-  TemplateRef
+  TemplateRef,
 } from '@angular/core';
-import {CalendarApp} from "@schedule-x/calendar";
-import {CustomComponentMeta, CustomComponentsMeta} from "../types/custom-components";
-import {createCustomComponentFn} from "../utils/create-custom-component-fn";
-import {CommonModule} from "@angular/common";
-import {SxPortalComponent} from "../utils/sx-portal.component";
+import { CalendarApp } from '@schedule-x/calendar';
+import {
+  CustomComponentMeta,
+  CustomComponentsMeta,
+} from '../types/custom-components';
+import { createCustomComponentFn } from '../utils/create-custom-component-fn';
+
+import { SxPortalComponent } from '../utils/sx-portal.component';
 
 export const randomStringId = () =>
-  's' + Math.random().toString(36).substring(2, 11)
+  's' + Math.random().toString(36).substring(2, 11);
 
 @Component({
-    selector: 'sx-calendar',
-    imports: [
-        CommonModule,
-        SxPortalComponent,
-    ],
-    template: `
+  selector: 'sx-calendar',
+  imports: [SxPortalComponent],
+  template: `
     <div [attr.id]="calendarElementId" class="ng-calendar-wrapper"></div>
 
-    <sx-portal
-      *ngFor="let comp of customComponentsMeta;"
-      [wrapperElement]="comp.wrapperElement"
-      [elementTag]="'div'"
-      [template]="getTemplate(comp.componentName)"
-      [props]="comp.props"
-    ></sx-portal>
+    @for (comp of customComponentsMeta; track comp) {
+      <sx-portal
+        [wrapperElement]="comp.wrapperElement"
+        [elementTag]="'div'"
+        [template]="getTemplate(comp.componentName)"
+        [props]="comp.props"
+      ></sx-portal>
+    }
   `,
-    styles: ``
+  styles: ``,
 })
 export class CalendarComponent implements AfterViewInit {
   @Input() calendarApp: CalendarApp;
@@ -40,148 +41,207 @@ export class CalendarComponent implements AfterViewInit {
   @ContentChild('dateGridEvent') dateGridEvent: TemplateRef<any>;
   @ContentChild('monthGridEvent') monthGridEvent: TemplateRef<any>;
   @ContentChild('monthAgendaEvent') monthAgendaEvent: TemplateRef<any>;
-  @ContentChild('headerContentLeftPrepend') headerContentLeftPrepend: TemplateRef<any>;
-  @ContentChild('headerContentLeftAppend') headerContentLeftAppend: TemplateRef<any>;
-  @ContentChild('headerContentRightPrepend') headerContentRightPrepend: TemplateRef<any>;
-  @ContentChild('headerContentRightAppend') headerContentRightAppend: TemplateRef<any>;
+  @ContentChild('headerContentLeftPrepend')
+  headerContentLeftPrepend: TemplateRef<any>;
+  @ContentChild('headerContentLeftAppend')
+  headerContentLeftAppend: TemplateRef<any>;
+  @ContentChild('headerContentRightPrepend')
+  headerContentRightPrepend: TemplateRef<any>;
+  @ContentChild('headerContentRightAppend')
+  headerContentRightAppend: TemplateRef<any>;
   @ContentChild('headerContent') headerContent: TemplateRef<any>;
   @ContentChild('eventModal') eventModal: TemplateRef<any>;
 
-  customComponentsMeta: CustomComponentsMeta = []
+  customComponentsMeta: CustomComponentsMeta = [];
 
   public calendarElementId = randomStringId();
 
   getTemplate(componentName: string): TemplateRef<any> {
-    if (componentName === 'timeGridEvent') return this.timeGridEvent
+    if (componentName === 'timeGridEvent') return this.timeGridEvent;
 
-    if (componentName === 'dateGridEvent') return this.dateGridEvent
+    if (componentName === 'dateGridEvent') return this.dateGridEvent;
 
-    if (componentName === 'monthGridEvent') return this.monthGridEvent
+    if (componentName === 'monthGridEvent') return this.monthGridEvent;
 
-    if (componentName === 'monthAgendaEvent') return this.monthAgendaEvent
+    if (componentName === 'monthAgendaEvent') return this.monthAgendaEvent;
 
-    if (componentName === 'eventModal') return this.eventModal
+    if (componentName === 'eventModal') return this.eventModal;
 
-    if (componentName === 'headerContentLeftPrepend') return this.headerContentLeftPrepend
+    if (componentName === 'headerContentLeftPrepend')
+      return this.headerContentLeftPrepend;
 
-    if (componentName === 'headerContentLeftAppend') return this.headerContentLeftAppend
+    if (componentName === 'headerContentLeftAppend')
+      return this.headerContentLeftAppend;
 
-    if (componentName === 'headerContentRightPrepend') return this.headerContentRightPrepend
+    if (componentName === 'headerContentRightPrepend')
+      return this.headerContentRightPrepend;
 
-    if (componentName === 'headerContentRightAppend') return this.headerContentRightAppend
+    if (componentName === 'headerContentRightAppend')
+      return this.headerContentRightAppend;
 
-    if (componentName === 'headerContent') return this.headerContent
+    if (componentName === 'headerContent') return this.headerContent;
 
-    throw new Error(`No template found for component name: ${componentName}`)
+    throw new Error(`No template found for component name: ${componentName}`);
   }
 
   ngAfterViewInit() {
-    if (typeof window !== 'object') return
+    if (typeof window !== 'object') return;
 
-    const calendarElement = document.getElementById(this.calendarElementId);
+    const calendarElement = document?.getElementById(this.calendarElementId);
     if (!(calendarElement instanceof HTMLElement)) {
-      throw new Error('No calendar element found')
+      throw new Error('No calendar element found');
     }
 
-    this.setCustomComponentFns()
-    this.calendarApp.render(calendarElement)
+    if (this.calendarApp == null) {
+      return;
+    }
+
+    this.setCustomComponentFns();
+    this.calendarApp?.render(calendarElement);
   }
 
   private setCustomComponentFns() {
     if (this.timeGridEvent) {
       this.calendarApp._setCustomComponentFn(
         'timeGridEvent',
-        createCustomComponentFn(this.setCustomComponentMeta.bind(this), this.timeGridEvent, 'timeGridEvent')
-      )
+        createCustomComponentFn(
+          this.setCustomComponentMeta.bind(this),
+          this.timeGridEvent,
+          'timeGridEvent',
+        ),
+      );
     }
 
     if (this.dateGridEvent) {
       this.calendarApp._setCustomComponentFn(
         'dateGridEvent',
-        createCustomComponentFn(this.setCustomComponentMeta.bind(this), this.dateGridEvent, 'dateGridEvent')
-      )
+        createCustomComponentFn(
+          this.setCustomComponentMeta.bind(this),
+          this.dateGridEvent,
+          'dateGridEvent',
+        ),
+      );
     }
 
     if (this.monthGridEvent) {
       this.calendarApp._setCustomComponentFn(
         'monthGridEvent',
-        createCustomComponentFn(this.setCustomComponentMeta.bind(this), this.monthGridEvent, 'monthGridEvent')
-      )
+        createCustomComponentFn(
+          this.setCustomComponentMeta.bind(this),
+          this.monthGridEvent,
+          'monthGridEvent',
+        ),
+      );
     }
 
     if (this.monthAgendaEvent) {
       this.calendarApp._setCustomComponentFn(
         'monthAgendaEvent',
-        createCustomComponentFn(this.setCustomComponentMeta.bind(this), this.monthAgendaEvent, 'monthAgendaEvent')
-      )
+        createCustomComponentFn(
+          this.setCustomComponentMeta.bind(this),
+          this.monthAgendaEvent,
+          'monthAgendaEvent',
+        ),
+      );
     }
 
     if (this.eventModal) {
       this.calendarApp._setCustomComponentFn(
         'eventModal',
-        createCustomComponentFn(this.setCustomComponentMeta.bind(this), this.eventModal, 'eventModal')
-      )
+        createCustomComponentFn(
+          this.setCustomComponentMeta.bind(this),
+          this.eventModal,
+          'eventModal',
+        ),
+      );
     }
 
     if (this.headerContentLeftPrepend) {
       this.calendarApp._setCustomComponentFn(
         'headerContentLeftPrepend',
-        createCustomComponentFn(this.setCustomComponentMeta.bind(this), this.headerContentLeftPrepend, 'headerContentLeftPrepend')
-      )
+        createCustomComponentFn(
+          this.setCustomComponentMeta.bind(this),
+          this.headerContentLeftPrepend,
+          'headerContentLeftPrepend',
+        ),
+      );
     }
 
     if (this.headerContentLeftAppend) {
       this.calendarApp._setCustomComponentFn(
         'headerContentLeftAppend',
-        createCustomComponentFn(this.setCustomComponentMeta.bind(this), this.headerContentLeftAppend, 'headerContentLeftAppend')
-      )
+        createCustomComponentFn(
+          this.setCustomComponentMeta.bind(this),
+          this.headerContentLeftAppend,
+          'headerContentLeftAppend',
+        ),
+      );
     }
 
     if (this.headerContentRightPrepend) {
       this.calendarApp._setCustomComponentFn(
         'headerContentRightPrepend',
-        createCustomComponentFn(this.setCustomComponentMeta.bind(this), this.headerContentRightPrepend, 'headerContentRightPrepend')
-      )
+        createCustomComponentFn(
+          this.setCustomComponentMeta.bind(this),
+          this.headerContentRightPrepend,
+          'headerContentRightPrepend',
+        ),
+      );
     }
 
     if (this.headerContentRightAppend) {
       this.calendarApp._setCustomComponentFn(
         'headerContentRightAppend',
-        createCustomComponentFn(this.setCustomComponentMeta.bind(this), this.headerContentRightAppend, 'headerContentRightAppend')
-      )
+        createCustomComponentFn(
+          this.setCustomComponentMeta.bind(this),
+          this.headerContentRightAppend,
+          'headerContentRightAppend',
+        ),
+      );
     }
 
     if (this.headerContent) {
       this.calendarApp._setCustomComponentFn(
         'headerContent',
-        createCustomComponentFn(this.setCustomComponentMeta.bind(this), this.headerContent, 'headerContent')
-      )
+        createCustomComponentFn(
+          this.setCustomComponentMeta.bind(this),
+          this.headerContent,
+          'headerContent',
+        ),
+      );
     }
   }
 
   setCustomComponentMeta = (component: CustomComponentMeta) => {
-    const wrapperWasDetached = !(component.wrapperElement instanceof HTMLElement);
-    if (wrapperWasDetached) return
+    const wrapperWasDetached = !(
+      component.wrapperElement instanceof HTMLElement
+    );
+    if (wrapperWasDetached) return;
 
-    const filterOutComponentsWithDetachedWrappers = ({ wrapperElement }: { wrapperElement: HTMLElement | null }) =>
-      wrapperElement instanceof HTMLElement
+    const filterOutComponentsWithDetachedWrappers = ({
+      wrapperElement,
+    }: {
+      wrapperElement: HTMLElement | null;
+    }) => wrapperElement instanceof HTMLElement;
     const newCustomComponents = [
-      ...this.customComponentsMeta.filter(filterOutComponentsWithDetachedWrappers)
-    ]
+      ...this.customComponentsMeta.filter(
+        filterOutComponentsWithDetachedWrappers,
+      ),
+    ];
 
-    const ccid = component.wrapperElement.dataset['ccid']
+    const ccid = component.wrapperElement.dataset['ccid'];
     const existingComponent = newCustomComponents.find(
-      ({ wrapperElement }) => wrapperElement.dataset['ccid'] === ccid
-    )
+      ({ wrapperElement }) => wrapperElement.dataset['ccid'] === ccid,
+    );
 
     if (existingComponent) {
-      existingComponent.wrapperElement.innerHTML = ''
+      existingComponent.wrapperElement.innerHTML = '';
       newCustomComponents.splice(
         newCustomComponents.indexOf(existingComponent),
-        1
-      )
+        1,
+      );
     }
 
-    this.customComponentsMeta = [...newCustomComponents, component]
-  }
+    this.customComponentsMeta = [...newCustomComponents, component];
+  };
 }
